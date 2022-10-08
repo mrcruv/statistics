ed9fc2579a4ed26a9058035c50bfe80008ffe2bd
Marco Ruvolo <marco.ruvolo@protonmail.com>
2022-10-08 12:34:24 +0200

    Load homework 2 (A3)
    simple CSV parser (matrix)

---
+++ simple_CSV_parser_cs/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace simple_CSV_parser_cs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 44);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "READ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(93, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 44);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "CLEAR";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 62);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(1072, 518);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(888, 35);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(196, 17);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "DOUBLE QUOTES AS DELIMITER";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(888, 12);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(178, 17);
+            this.checkBox2.TabIndex = 4;
+            this.checkBox2.Text = "MAKE STRINGS LOWERCASE";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1096, 592);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "A3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+    }
+}
+
---
+++ simple_CSV_parser_cs/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Windows.Forms;
+
+namespace simple_CSV_parser_cs
+{
+    public partial class Form1 : Form
+    {
+        private readonly char separator = ',';
+        private string[] lines;
+        private string file_name = "data.csv";
+        private int n_data = 0;
+        private int n_variables = 0;
+        private string[] header;
+        private string[,] data;
+        private static readonly bool sanitizeDefault = false;
+        private bool sanitize = sanitizeDefault;
+        private static readonly bool lowercaseDefault = true;
+        private bool lowercase = lowercaseDefault;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private static string SanitizeString(string str)
+        {
+            var result = str.Trim();
+            var length = result.Length;
+            if (length >= 2)
+            {
+                result = str.Remove(length - 1, 1);
+                result = result.Remove(0, 1);
+            }
+            return result;
+        }
+
+        private static string EscapeDoubleQuotes(string str)
+        {
+            var result = str.Replace("\"\"", "\"");
+            return result;
+        }
+
+        private static string ProcessString(string str, bool sanitize, bool lowercase)
+        {
+            var result = str;
+            if (sanitize)
+            {
+                result = SanitizeString(result);
+            }
+            if (lowercase)
+            {
+                result = result.ToLower();
+            }
+            result = EscapeDoubleQuotes(result);
+            return result;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.lines = File.ReadAllLines(this.file_name);
+            this.n_data = this.lines.Count() - 1;
+            this.header = this.lines[0].Split(this.separator);
+            this.n_variables = this.header.Count();
+            this.data = new string[this.n_data, this.n_variables];
+            this.richTextBox1.Text += "# VARIABLES: " + this.n_variables + "\n";
+            this.richTextBox1.Text += "# DATA: " + this.n_data + "\n";
+            this.richTextBox1.Text += "HEADER: ";
+            for (int i = 0; i < this.n_variables - 1; i++)
+            {
+                this.header[i] = ProcessString(this.header[i], this.sanitize, this.lowercase);
+                this.richTextBox1.Text += this.header[i] + ", ";
+            }
+            this.header[this.n_variables - 1] = ProcessString(this.header[this.n_variables - 1], this.sanitize, this.lowercase);
+            this.richTextBox1.Text += this.header[this.n_variables - 1] + "\n";
+            for (int i = 0; i < this.n_data; i++)
+            {
+                var current_line = this.lines[i + 1].Split(this.separator);
+                for (int j = 0; j < this.n_variables - 1; j++)
+                {
+                    this.data[i, j] = ProcessString(current_line[j], this.sanitize, this.lowercase);
+                    this.richTextBox1.Text += this.data[i, j] + ", ";
+                }
+                this.data[i, this.n_variables - 1] = ProcessString(current_line[this.n_variables - 1], this.sanitize, this.lowercase);
+                this.richTextBox1.Text += this.data[i, this.n_variables - 1] + "\n";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.richTextBox1.Clear();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            this.sanitize = checkBox1.Checked;
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            this.lowercase = checkBox2.Checked;
+        }
+    }
+}
