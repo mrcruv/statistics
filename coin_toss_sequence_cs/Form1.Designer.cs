3bf3d175c79536d18130f04bf54a91ee809d61f4
Marco Ruvolo <marco.ruvolo@protonmail.com>
2022-10-25 21:12:57 +0200

    Update homework 4 (A6, A7)
    minor fix and improvements

--- coin_toss_sequence_cs/Form1.Designer.cs
+++ coin_toss_sequence_cs/Form1.Designer.cs
@@ -36,17 +36,23 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
             this.pictureBox4 = new System.Windows.Forms.PictureBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
@@ -103,48 +109,30 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(795, 12);
+            this.label1.Location = new System.Drawing.Point(515, 13);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(87, 15);
+            this.label1.Size = new System.Drawing.Size(111, 15);
             this.label1.TabIndex = 7;
-            this.label1.Text = "# COIN TOSSES";
+            this.label1.Text = "# COIN TOSSES: 100";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(625, 12);
+            this.label2.Location = new System.Drawing.Point(319, 13);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(128, 15);
+            this.label2.Size = new System.Drawing.Size(156, 15);
             this.label2.TabIndex = 8;
-            this.label2.Text = "SUCCESS PROBABILITY";
+            this.label2.Text = "SUCCESS PROBABILITY: 50%";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // richTextBox1
             // 
             this.richTextBox1.Location = new System.Drawing.Point(319, 407);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(606, 256);
+            this.richTextBox1.Size = new System.Drawing.Size(606, 226);
             this.richTextBox1.TabIndex = 9;
             this.richTextBox1.Text = "";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(625, 30);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(130, 23);
-            this.textBox1.TabIndex = 10;
-            this.textBox1.Text = "50";
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(795, 30);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(130, 23);
-            this.textBox2.TabIndex = 11;
-            this.textBox2.Text = "100";
-            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // pictureBox4
             // 
@@ -186,24 +174,75 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(67, 389);
+            this.label6.Location = new System.Drawing.Point(93, 389);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(194, 15);
+            this.label6.Size = new System.Drawing.Size(124, 15);
             this.label6.TabIndex = 18;
-            this.label6.Text = "SUCCESS/UNSUCCESS HISTOGRAM";
+            this.label6.Text = "SUCCESS HISTOGRAM";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(710, 12);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(99, 15);
+            this.label7.TabIndex = 20;
+            this.label7.Text = "# SEQUENCES: 50";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(515, 31);
+            this.trackBar1.Maximum = 1000;
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(104, 45);
+            this.trackBar1.TabIndex = 21;
+            this.trackBar1.Value = 100;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(319, 31);
+            this.trackBar2.Maximum = 100;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(104, 45);
+            this.trackBar2.TabIndex = 22;
+            this.trackBar2.Value = 50;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.Location = new System.Drawing.Point(705, 31);
+            this.trackBar3.Maximum = 1000;
+            this.trackBar3.Minimum = 1;
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Size = new System.Drawing.Size(104, 45);
+            this.trackBar3.TabIndex = 23;
+            this.trackBar3.Value = 50;
+            this.trackBar3.Scroll += new System.EventHandler(this.trackBar3_Scroll);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(318, 639);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(607, 23);
+            this.progressBar1.TabIndex = 24;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(937, 675);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.trackBar3);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.pictureBox4);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -218,6 +257,9 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -233,12 +275,15 @@
         private Label label1;
         private Label label2;
         private RichTextBox richTextBox1;
-        private TextBox textBox1;
-        private TextBox textBox2;
         private PictureBox pictureBox4;
         private Label label3;
         private Label label4;
         private Label label5;
         private Label label6;
+        private Label label7;
+        private TrackBar trackBar1;
+        private TrackBar trackBar2;
+        private TrackBar trackBar3;
+        private ProgressBar progressBar1;
     }
 }
--- coin_toss_sequence_cs/Form1.cs
+++ coin_toss_sequence_cs/Form1.cs
@@ -1,28 +1,56 @@
+using System.Collections;
+using System.Collections.Specialized;
+using System.ComponentModel.DataAnnotations;
+using System.Drawing.Drawing2D;
 using System.Reflection;
+using System.Xml.Linq;
 
 namespace coin_toss_sequence_cs
 {
     public partial class Form1 : Form
     {
-        private int n_tosses;
+        private int n_tosses, n_sequence;
         private double p_success;
-        private Random generator = new Random();
+        private readonly Random generator = new Random();
         private Bitmap bitmap1, bitmap2, bitmap3, bitmap4;
         private Graphics graphics1, graphics2, graphics3, graphics4;
+        private readonly byte type = 1;
         private readonly string log_delimiter = "*****";
         public Form1()
         {
             InitializeComponent();
         }
 
+        private void initialize_attributes()
+        {
+            this.n_tosses = this.trackBar1.Value;
+            this.p_success = (double)(this.trackBar2.Value) / 100;
+            this.n_sequence = this.trackBar3.Value;
+            this.bitmap1 = new Bitmap(this.pictureBox1.Width, this.pictureBox1.Height);
+            this.graphics1 = Graphics.FromImage(this.bitmap1);
+            this.bitmap2 = new Bitmap(this.pictureBox2.Width, this.pictureBox2.Height);
+            this.graphics2 = Graphics.FromImage(this.bitmap2);
+            this.bitmap3 = new Bitmap(this.pictureBox3.Width, this.pictureBox3.Height);
+            this.graphics3 = Graphics.FromImage(this.bitmap3);
+            this.bitmap4 = new Bitmap(this.pictureBox4.Width, this.pictureBox4.Height);
+            this.graphics4 = Graphics.FromImage(this.bitmap4);
+        }
+
+        private void clear()
+        {
+            this.pictureBox1.Image = this.bitmap1;
+            this.pictureBox1.Refresh();
+            this.pictureBox2.Image = this.bitmap2;
+            this.pictureBox2.Refresh();
+            this.pictureBox3.Image = this.bitmap3;
+            this.pictureBox3.Refresh();
+            this.pictureBox4.Image = this.bitmap4;
+            this.pictureBox4.Refresh();
+        }
+
         private float transform_x(float x, float x_min, float x_max, float width)
         {
             return ((float)(x - x_min) / (x_max - x_min)) * width;
-        }
-
-        private void textBox2_TextChanged(object sender, EventArgs e)
-        {
-
         }
 
         private float transform_y(float y, float y_min, float y_max, float height)
@@ -46,60 +74,102 @@ namespace coin_toss_sequence_cs
 
         private void button1_Click(object sender, EventArgs e)
         {
-            this.bitmap1 = new Bitmap(this.pictureBox1.Width, this.pictureBox1.Height);
-            this.graphics1 = Graphics.FromImage(this.bitmap1);
-            this.bitmap2 = new Bitmap(this.pictureBox2.Width, this.pictureBox2.Height);
-            this.graphics2 = Graphics.FromImage(this.bitmap2);
-            this.bitmap3 = new Bitmap(this.pictureBox3.Width, this.pictureBox3.Height);
-            this.graphics3 = Graphics.FromImage(this.bitmap3);
-            this.bitmap4 = new Bitmap(this.pictureBox4.Width, this.pictureBox4.Height);
-            this.graphics4 = Graphics.FromImage(this.bitmap4);
-            this.n_tosses = int.Parse(this.textBox2.Text);
-            this.p_success = double.Parse(this.textBox1.Text) / 100;
-            var n_success = 0;
-            this.richTextBox1.Text += $"{this.log_delimiter} begin coin tossing and frequency computation (p = {this.p_success}; n = {this.n_tosses}) {this.log_delimiter}\n";
-            for (int i = 0; i < this.n_tosses; i++)
+            this.initialize_attributes();
+            int[] n_success = new int[this.n_sequence], n_unsuccess = new int[this.n_sequence];
+            this.progressBar1.Value = this.progressBar1.Minimum;
+
+            for (int j = 0; j < this.n_sequence; j++)
             {
-                n_success += (int)this.toss();
-                var x1 = this.transform_x(i, 0, this.n_tosses, this.pictureBox1.Width);
-                var x2 = x1;
-                var y1 = this.transform_y(0, 0, this.n_tosses, this.pictureBox1.Height);
-                var y2 = this.transform_y(n_success, 0, this.n_tosses, this.pictureBox1.Height);
-                this.graphics1.DrawLine(Pens.Black, x1, y1, x2, y2);
+                this.richTextBox1.Text += $"{this.log_delimiter} ({j + 1}) begin coin tossing and frequency computation (p = {this.p_success}; n = {this.n_tosses}) {this.log_delimiter}\n";
+                n_success[j] = 0;
+                var points1 = new List<PointF>();
+                var points2 = new List<PointF>();
+                var points3 = new List<PointF>();
+                var types = new List<byte>();
+                for (int i = 0; i < this.n_tosses; i++)
+                {
+                    n_success[j] += (int)this.toss();
 
-                x1 = this.transform_x(i, 0, this.n_tosses, this.pictureBox2.Width);
-                x2 = x1;
-                y1 = this.transform_y(0, 0, 1, this.pictureBox2.Height);
-                y2 = this.transform_y((float)n_success / (i + 1), 0, 1, this.pictureBox2.Height);
-                this.graphics2.DrawLine(Pens.Black, x1, y1, x2, y2);
+                    var x1 = this.transform_x(i, 0, this.n_tosses, this.pictureBox1.Width);
+                    var y1 = this.transform_y(n_success[j], 0, this.n_tosses, this.pictureBox1.Height);
+                    points1.Add(new PointF(x1, y1));
 
-                x1 = this.transform_x(i, 0, this.n_tosses, this.pictureBox3.Width);
-                x2 = x1;
-                y1 = this.transform_y(0, 0, (float)Math.Sqrt(this.n_tosses), this.pictureBox3.Height);
-                y2 = this.transform_y(n_success / (float)Math.Sqrt(i + 1), 0, (float)Math.Sqrt(this.n_tosses), this.pictureBox3.Height);
-                this.graphics3.DrawLine(Pens.Black, x1, y1, x2, (float)(y2 / (Math.Sqrt(i + 1))));
+                    var x2 = this.transform_x(i, 0, this.n_tosses, this.pictureBox2.Width);
+                    var y2 = this.transform_y((float)n_success[j] / (i + 1), 0, 1, this.pictureBox2.Height);
+                    points2.Add(new PointF(x2, y2));
+
+                    var x3 = this.transform_x(i, 0, this.n_tosses, this.pictureBox3.Width);
+                    var y3 = this.transform_y(n_success[j] / (float)Math.Sqrt(i + 1), 0, (float)Math.Sqrt(this.n_tosses), this.pictureBox3.Height);
+                    points3.Add(new PointF(x3, y3));
+
+                    types.Add(this.type);
+                }
+                n_unsuccess[j] = this.n_tosses - n_success[j];
+
+                var graphics_path1 = new GraphicsPath(points1.ToArray(), types.ToArray());
+                this.graphics1.DrawPath(Pens.Black, graphics_path1);
+
+                var graphics_path2 = new GraphicsPath(points2.ToArray(), types.ToArray());
+                this.graphics2.DrawPath(Pens.Black, graphics_path2);
+
+                var graphics_path3 = new GraphicsPath(points3.ToArray(), types.ToArray());
+                this.graphics3.DrawPath(Pens.Black, graphics_path3);
+
+                this.richTextBox1.Text += $"{this.log_delimiter} ({j + 1}) frequency succesfully computed (#success = {n_success[j]}; #unsuccess = {n_unsuccess[j]}) {this.log_delimiter}\n";
+                this.progressBar1.Value = (int)Math.Round(((double)(j + 1) / (this.n_sequence)) * this.progressBar1.Maximum);
             }
-            var n_unsuccess = this.n_tosses - n_success;
-            this.richTextBox1.Text += $"{this.log_delimiter} frequency succesfully computed (#success = {n_success}; #unsuccess = {n_unsuccess}) {this.log_delimiter}\n";
             this.richTextBox1.Text += $"{this.log_delimiter} begin histogram computation {this.log_delimiter}\n";
-            var bar_width = this.pictureBox4.Width / 10;
-            var center = (int)((float)this.pictureBox4.Width / 2);
-            var success_height = (int)(((float)n_success / this.n_tosses) * this.pictureBox4.Height);
-            var unsuccess_height = (int)(((float)n_unsuccess / this.n_tosses) * this.pictureBox4.Height);
-            var success_bar = new Rectangle(center - bar_width, this.pictureBox4.Height - success_height, bar_width, success_height);
-            var unsuccess_bar = new Rectangle(center, this.pictureBox4.Height - unsuccess_height, bar_width, unsuccess_height);
-            this.graphics4.DrawRectangle(Pens.Black, success_bar);
-            this.graphics4.DrawRectangle(Pens.Black, unsuccess_bar);
-            this.richTextBox1.Text += $"{this.log_delimiter} histogram succesfully computed {this.log_delimiter}\n";
 
-            this.pictureBox1.Image = this.bitmap1;
-            this.pictureBox1.Refresh();
-            this.pictureBox2.Image = this.bitmap2;
-            this.pictureBox2.Refresh();
-            this.pictureBox3.Image = this.bitmap3;
-            this.pictureBox3.Refresh();
-            this.pictureBox4.Image = this.bitmap4;
-            this.pictureBox4.Refresh();
+            Hashtable frequencies = new Hashtable();
+            this.progressBar1.Value = this.progressBar1.Minimum;
+
+            for (int i = 0; i < this.n_sequence; i++)
+            {
+                var current_value = n_success[i];
+                if (frequencies.ContainsKey(current_value))
+                {
+                    frequencies[current_value] = (int)frequencies[current_value] + 1;
+                }
+                else
+                {
+                    frequencies.Add(current_value, 1);
+                }
+                this.progressBar1.Value = (int)Math.Round(((double)(i + 1) / (this.n_sequence)) * this.progressBar1.Maximum);
+            }
+
+            var distinct_success = n_success.Distinct().OrderBy(e => e).ToArray();
+            var n_distinct_success = distinct_success.Count();
+            var bar_width = this.pictureBox4.Width / n_distinct_success;
+            this.progressBar1.Value = this.progressBar1.Minimum;
+            for (int i = 0; i < n_distinct_success; i++)
+            {
+                var index = distinct_success[i];
+                var count_sequence = (int)frequencies[index];
+                var success_height = (int)Math.Round(((float)count_sequence / this.n_sequence) * this.pictureBox4.Height);
+                var success_bar = new Rectangle(bar_width * i, this.pictureBox4.Height - success_height, bar_width, success_height);
+                this.graphics4.DrawRectangle(Pens.Black, success_bar);
+                this.progressBar1.Value = (int)Math.Round(((double)(i + 1) / (n_distinct_success)) * this.progressBar1.Maximum);
+            }
+            this.richTextBox1.Text += $"{this.log_delimiter} histogram succesfully computed {this.log_delimiter}\n";
+            this.clear();
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            this.label1.Text = "# COIN TOSSES: " + this.trackBar1.Value;
+            this.n_tosses = this.trackBar1.Value;
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            this.label2.Text = "SUCCESS PROBABILITY: " + this.trackBar2.Value + "%";
+            this.p_success = (double)(this.trackBar2.Value) / 100;
+        }
+
+        private void trackBar3_Scroll(object sender, EventArgs e)
+        {
+            this.label7.Text = "# SEQUENCES: " + this.trackBar3.Value;
+            this.n_sequence = this.trackBar3.Value;
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -113,6 +183,7 @@ namespace coin_toss_sequence_cs
             this.pictureBox4.Image = null;
             this.pictureBox4.Refresh();
             this.richTextBox1.Clear();
+            this.progressBar1.Value = this.progressBar1.Minimum;
         }
 
         private void pictureBox3_Click(object sender, EventArgs e)
@@ -126,16 +197,6 @@ namespace coin_toss_sequence_cs
         }
 
         private void label2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
         }
